c44bb6e16e70aeccbb3fbca61a042d47f38d1527
ptorrey <none@none>
2015-01-11 09:26:54 -0800

    printf

--- galaxy_sf/blackholes/blackhole_swallow_and_kick.c
+++ galaxy_sf/blackholes/blackhole_swallow_and_kick.c
@@ -369,6 +369,8 @@ int blackhole_swallow_and_kick_evaluate(int target, int mode, int *nexport, int 
                 /* we've found a particle to be swallowed.  This could a BH merger, DM particle, or baryon w/ feedback */
                 if(P[j].SwallowID == id)
                 {
+                    printf("found particle P[j].ID = %llu with P[j].SwallowID = %llu of type P[j].Type = %d nearby id = %llu \n",
+                           (unsigned long long) P[j].ID, P[j].SwalowID, P[j].Type, id);
                     /* this is a BH-BH merger */
                     if(P[j].Type == 5)
                     {
