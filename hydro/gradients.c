bd85e3d561ce9de3ae2cbca3765d7c800319efeb
Philip Hopkins <phopkins@caltech.edu>
2016-05-28 17:15:49 -0700

    improved flux-limiter for timestep control for high CR diffusion coefficients

--- hydro/gradients.c
+++ hydro/gradients.c
@@ -1575,16 +1575,20 @@ void hydro_gradient_calc(void)
 #endif
                 SphP[i].CosmicRayDiffusionCoeff += CR_kappa_streaming;
                 /* now we apply a limiter to prevent the coefficient from becoming too large: cosmic rays cannot stream/diffuse with v_diff > c */
-                double diffusion_velocity_limit = 0.1 * C; /* maximum diffusion velocity (set <C if desired) */
-                double kappa_diff_vel = SphP[i].CosmicRayDiffusionCoeff * GAMMA_COSMICRAY_MINUS1 / CRPressureGradScaleLength * All.UnitVelocity_in_cm_per_s;
+                double diffusion_velocity_limit = 1.0 * C; /* maximum diffusion velocity (set <C if desired) */
+#ifdef GALSF
+                diffusion_velocity_limit = 0.01 * C;
+#endif
+                double Lscale = DMIN(Get_Particle_Size(i)*All.cf_atime , CRPressureGradScaleLength);
+                double kappa_diff_vel = SphP[i].CosmicRayDiffusionCoeff * GAMMA_COSMICRAY_MINUS1 / Lscale * All.UnitVelocity_in_cm_per_s;
                 SphP[i].CosmicRayDiffusionCoeff *= 1 / (1 + kappa_diff_vel/diffusion_velocity_limit); /* caps maximum here */
                 if((SphP[i].CosmicRayDiffusionCoeff<=0)||(isnan(SphP[i].CosmicRayDiffusionCoeff))) {SphP[i].CosmicRayDiffusionCoeff=0;}
 #ifdef GALSF
                 /* for multi-physics problems, we suppress diffusion where it is irrelevant */
                 {
                     double P_cr_Ratio = Get_Particle_CosmicRayPressure(i) / (MIN_REAL_NUMBER + SphP[i].Pressure);
-                    double P_min = 1.0e-2; if(P_cr_Ratio < P_min) {SphP[i].CosmicRayDiffusionCoeff *= pow(P_cr_Ratio/P_min,2);}
-                    P_min = 1.0e-4; if(P_cr_Ratio < P_min) {SphP[i].CosmicRayDiffusionCoeff *= pow(P_cr_Ratio/P_min,2);}
+                    double P_min = 3.0e-2; if(P_cr_Ratio < P_min) {SphP[i].CosmicRayDiffusionCoeff *= pow(P_cr_Ratio/P_min,2);}
+                    P_min = 1.0e-3; if(P_cr_Ratio < P_min) {SphP[i].CosmicRayDiffusionCoeff *= pow(P_cr_Ratio/P_min,2);}
                 }
 #endif
             } else {
