852a8b052bcd51dfaccee4cfc09f6c116afba48d
Michael Grudic <mgrudich@caltech.edu>
2018-11-19 07:18:37 +0000

    Template_Config.sh edited online with Bitbucket

--- Template_Config.sh
+++ Template_Config.sh
@@ -101,7 +101,7 @@
 #COOLING                        # enables radiative cooling and heating: if GALSF, also external UV background read from file "TREECOOL" (included in the cooling folder)
 #COOL_LOW_TEMPERATURES          # allow fine-structure and molecular cooling to ~10 K; account for optical thickness and line-trapping effects with proper opacities
 #COOL_METAL_LINES_BY_SPECIES    # use full multi-species-dependent cooling tables ( http://www.tapir.caltech.edu/~phopkins/public/spcool_tables.tgz, or the Bitbucket site); requires METALS on; cite Wiersma et al. 2009 (MNRAS, 393, 99) in addition to Hopkins et al. 2017 (arXiv:1702.06148)
-COOL_GRACKLE                   # enable Grackle: cooling+chemistry package (requires COOLING above; https://grackle.readthedocs.org/en/latest ); see Grackle code for their required citations
+#COOL_GRACKLE                   # enable Grackle: cooling+chemistry package (requires COOLING above; https://grackle.readthedocs.org/en/latest ); see Grackle code for their required citations
 #COOL_GRACKLE_CHEMISTRY=1       # choose Grackle cooling chemistry: (0)=tabular, (1)=Atomic, (2)=(1)+H2+H2I+H2II, (3)=(2)+DI+DII+HD
 #METALS                         # enable metallicities (with multiple species optional) for gas and stars [must be included in ICs or injected via dynamical feedback; needed for some routines]
 ## ----------------------------------------------------------------------------------------------------
@@ -395,7 +395,7 @@ COOL_GRACKLE                   # enable Grackle: cooling+chemistry package (requ
 ####################################################################################################
 # --------------------
 # ----- General De-Bugging and Special Behaviors
-DEVELOPER_MODE                 # allows you to modify various numerical parameters (courant factor, etc) at run-time
+#DEVELOPER_MODE                 # allows you to modify various numerical parameters (courant factor, etc) at run-time
 #STOP_WHEN_BELOW_MINTIMESTEP    # forces code to quit when stepsize wants to go below MinSizeTimestep specified in the parameterfile
 #DEBUG                          # enables core-dumps and FPU exceptions
 # --------------------
