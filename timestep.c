2da998adb161951923e3dc4cd5fc35cd5ed16ee2
guszejnov <none@none>
2018-11-20 15:37:26 -0600

    Fixed typo in timestep.c gravtree.c Tidalcorrection put behind flag

--- timestep.c
+++ timestep.c
@@ -936,7 +936,7 @@ integertime get_timestep(int p,		/*!< particle index */
 
         if(dt > dt_ngbs && dt_ngbs > 0) {dt = 1.01 * dt_ngbs; }
 #ifdef SINGLE_STAR_FORMATION
-	if(P[p].DensAroundStar) {double eps = DMAX(AllForceSoftening[5], BPP(p).Hsml * KERNEL_CORE_SIZE); //BPP(p).BH_NearestGasNeighbor; //DMAX(BPP(p).BH_NearestGasNeighbor, All.ForceSoftening[5]);
+	if(P[p].DensAroundStar) {double eps = DMAX(All.ForceSoftening[5], BPP(p).Hsml * KERNEL_CORE_SIZE); //BPP(p).BH_NearestGasNeighbor; //DMAX(BPP(p).BH_NearestGasNeighbor, All.ForceSoftening[5]);
 	double dt_gas = sqrt(All.ErrTolIntAccuracy * All.cf_atime * eps * eps * eps/ All.G / P[p].Mass); // fraction of the freefall time of the nearest gas particle from rest
 	if(dt > dt_gas && dt_gas > 0) {dt = 1.01 * dt_gas; }}
 
