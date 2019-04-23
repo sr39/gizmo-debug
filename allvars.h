532928d62370fa8e2c83c4737ed1e96f5454e6bb
Mike Grudic <mgrudich@caltech.edu>
2019-04-23 09:01:54 -0700

    added flag for super timestep - 0 if allowed, 1 if not allowed

--- allvars.h
+++ allvars.h
@@ -2182,6 +2182,7 @@ extern ALIGN(32) struct particle_data
     MyFloat min_bh_periastron;
     MyFloat min_bh_approach_time;
 #ifdef SINGLE_STAR_SUPERTIMESTEPPING
+    int SuperTimestepFlag; // 1 if allowed to super-timestep, 0 otherwise
     MyDouble comp_Pos[3]; //position of binary companion
     MyDouble comp_Vel[3]; //velocity of binary companion
     MyDouble comp_Mass; //mass of binary companion
