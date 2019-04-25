d507eb9d2583689ebbf0978b5f7502bae171719e
Mike Grudic <mgrudich@caltech.edu>
2019-04-25 10:21:05 -0500

    added nbody header file

---
+++ nbody/nbody.h
@@ -0,0 +1,9 @@
+/* File nbody.h
+Contains function prototypes for routines used to do sub-cycled few-N-body evolution in star cluster simulations
+ */
+
+#ifndef gizmo_nbody_h
+#defined gizmo_nbody_h
+
+void kepler_timestep(int i, double dt, double kick_dv[3], double drift_dx[3], int mode);
+double eccentric_anomaly(double mean_anomaly, double ecc);
