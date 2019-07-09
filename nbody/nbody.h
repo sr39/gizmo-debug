8435d6135cb708cd7a3fdeb552a792df2c8651c9
Mike Grudic <mgrudich@caltech.edu>
2019-07-08 18:09:15 -0700

    Hermite integration working; tested in 2-body problem

--- nbody/nbody.h
+++ nbody/nbody.h
@@ -7,4 +7,7 @@ Contains function prototypes for routines used to do sub-cycled few-N-body evolu
 #endif
 
 void kepler_timestep(int i, double dt, double kick_dv[3], double drift_dx[3], int mode);
+double gravfac(double r, double mass);
+double gravfac2(double r, double mass);
+void grav_accel_jerk(double mass, double dx[3], double dv[3], double accel[3], double jerk[3]);
 double eccentric_anomaly(double mean_anomaly, double ecc);
