9d5c7dad59e5797173692070709f1717440f75f7
Mike Grudic <mgrudich@caltech.edu>
2019-08-15 06:49:13 -0700

    Fixed bugs in initialization of BH_Specific_AngMom for star formation problems. Allows accretion prescription invoking circularization radius to work correctly; both SLOPE1 and SLOPE2 settings run the SIS correctly

--- galaxy_sf/blackholes/blackhole.c
+++ galaxy_sf/blackholes/blackhole.c
@@ -495,17 +495,19 @@ void set_blackhole_mdot(int i, int n, double dt)
         double t_yr = SEC_PER_YEAR / (All.UnitTime_in_s / All.HubbleParam);
         double t_acc_disk = 4.2e7 * t_yr * pow((BPP(n).BH_Mass_AlphaDisk+BPP(n).BH_Mass) / BPP(n).BH_Mass_AlphaDisk, 0.4); /* shakura-sunyaev disk, integrated out to Q~1 radius, approximately */
 #ifdef SINGLE_STAR_SINK_DYNAMICS
-        double Gm_i=1./(All.G*P[n].Mass), t_dyn_eff=sqrt(PPP[n].Hsml*PPP[n].Hsml*PPP[n].Hsml*Gm_i); // dynamical time at radius "H" where the neighbor gas is located
+        double Gm_i=1./(All.G*P[n].Mass), reff = DMAX(All.SofteningTable[5],Get_Particle_Size(n)), t_dyn_eff =sqrt(reff*reff*reff*Gm_i); // dynamical time at radius "H" where the neighbor gas is located
 #if defined(BH_FOLLOW_ACCRETED_ANGMOM)
         double j=0; for(k=0;k<3;k++) {j+=P[n].BH_Specific_AngMom[k]*P[n].BH_Specific_AngMom[k];} // calculate magnitude of specific ang mom
         if(j>0) {j=sqrt(j);} else {j=0;}
-        t_dyn_eff = DMIN( t_dyn_eff , j*j*j*Gm_i*Gm_i ); // dynamical time at circularization radius of the alpha-disk
+        t_acc_disk = DMIN(t_dyn_eff, j*j*j*Gm_i*Gm_i ); // dynamical time at circularization radius of the alpha-disk
 #endif
-        t_acc_disk = 10. * 2.*M_PI * t_dyn_eff; // 10 orbits at circularization radius to spiral all the way in (very fast)
+        t_acc_disk = 10. * 2.*M_PI * t_acc_disk; // 10 orbits at circularization radius to spiral all the way in (very fast)
+	t_acc_disk = DMAX(t_acc_disk, t_dyn_eff);
 #endif
 #if defined(BH_GRAVCAPTURE_GAS)
         t_acc_disk /= All.BlackHoleAccretionFactor; // when using GRAVCAPTURE, this won't multiply the continuous mdot, but rather mdot from disk to BH
 #endif
+
         t_acc_disk = DMAX(t_acc_disk , 3.*dt); /* make sure accretion timescale is at least a few timesteps to avoid over-shoot, etc */
         mdot = BPP(n).BH_Mass_AlphaDisk / t_acc_disk;
     }
