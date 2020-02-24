d583f204ba99cb321b1904af6eb774dfb1dadc84
Mike Grudic <mgrudich@caltech.edu>
2020-02-24 07:29:07 -0800

    tweak to accretion criteria specific to gas

--- galaxy_sf/blackholes/blackhole.c
+++ galaxy_sf/blackholes/blackhole.c
@@ -107,11 +107,13 @@ int bh_check_boundedness(int j, double vrel, double vesc, double dr_code, double
     /* if pair is a gas particle make sure to account for its thermal pressure */
     double cs = 0; if(P[j].Type==0) {cs=Particle_effective_soundspeed_i(j);}
 #if defined(SINGLE_STAR_SINK_DYNAMICS) && defined(MAGNETIC)
-    double bmag=0; for(int k=0;k<3;k++) {bmag+=Get_Particle_BField(j,k)*Get_Particle_BField(j,k);}
-    cs = sqrt(cs*cs + bmag/SphP[j].Density);
+    if(P[j].Type == 0) {
+        double bmag=0; for(int k=0;k<3;k++) {bmag+=Get_Particle_BField(j,k)*Get_Particle_BField(j,k);}
+        cs = sqrt(cs*cs + bmag/SphP[j].Density);
+    }
 #endif    
 #ifdef SINGLE_STAR_SINK_DYNAMICS
-    if(Get_Particle_Size(j) > sink_radius * 1.396263) return 0; // particle volume should be less than sink volume, enforcing a minimum spatial resolution around the sink
+    if(P[j].Type == 0) {if(Get_Particle_Size(j) > sink_radius * 1.396263) return 0;} // particle volume should be less than sink volume, enforcing a minimum spatial resolution around the sink
 #endif
 #if defined(COOLING) && defined(SINGLE_STAR_SINK_DYNAMICS)
     double nHcgs = HYDROGEN_MASSFRAC * (SphP[j].Density * All.cf_a3inv * All.UnitDensity_in_cgs * All.HubbleParam * All.HubbleParam) / PROTONMASS;
