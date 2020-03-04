48b08e50187c80a618c9ce490474ee57b2adea82
David Guszejnov <guszejnov.david@gmail.com>
2020-03-04 12:46:13 -0600

    Now we turn the ORION-lik PS evolution module on when using either SINGLE_STAR_FB_RT_HEATING or SINGLE_STAR_FB_JETS Reverts back to SINGLE_STAR_PROTOSTELLAR_EVOLUTION 0 if using SINGLE_STAR_PROMOTION

--- allvars.h
+++ allvars.h
@@ -2399,10 +2399,9 @@ extern ALIGN(32) struct particle_data
 #endif  
 #endif
 
-#if ( (!defined(SINGLE_STAR_PROTOSTELLAR_EVOLUTION)) && defined(SINGLE_STAR_FB_RT_HEATING) )
+#if ( (!defined(SINGLE_STAR_PROTOSTELLAR_EVOLUTION)) && (defined(SINGLE_STAR_FB_RT_HEATING) || defined(SINGLE_STAR_FB_JETS)) )
 #ifndef SINGLE_STAR_PROMOTION
-//#define SINGLE_STAR_PROTOSTELLAR_EVOLUTION 1 //default
-#define SINGLE_STAR_PROTOSTELLAR_EVOLUTION 0 //default
+#define SINGLE_STAR_PROTOSTELLAR_EVOLUTION 1 //default PS evolution based on ORION module
 #else
 #define SINGLE_STAR_PROTOSTELLAR_EVOLUTION 0 // the promotion module is incompatible with the evolution model from ORION we use in SINGLE_STAR_PROTOSTELLAR_EVOLUTION 1, so we revert to the simpler one
 #endif 
