f5f50a5b5d9931e1145d4284574139701e38815c
Philip Hopkins <phopkins@caltech.edu>
2020-07-03 20:50:29 -0700

    argh pushed the wrong version, this is it - take 4

--- eos/cosmic_ray_fluid/cosmic_ray_utilities.c
+++ eos/cosmic_ray_fluid/cosmic_ray_utilities.c
@@ -1256,7 +1256,7 @@ double return_CRbin_kinetic_energy_in_GeV_binvalsNRR(int k_CRegy)
     if(CR_check_if_bin_is_nonrelativistic(k_CRegy))
     {
         double fac = 0.5328928536929374; // converts from R_GV to E in GeV for E = p^2/(2m), assuming Z=1, m=mp
-        if(Zabs>=1.01) {fac /= 2.*Zabs*Zabs*Zabs;} // assume A = 2.*Z
+        if(Zabs>=1.01) {fac *= Zabs/2.;} // assume A = 2.*Z
         return fac * R_GV*R_GV; // E in GeV in non-relativistic limit
     }
     return R_GV / Zabs; // E in GeV in relativistic limit
