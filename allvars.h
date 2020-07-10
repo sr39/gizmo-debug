a30f77ec72866947dcd0a7ebd31928d470e3a0d5
Philip Hopkins <phopkins@caltech.edu>
2020-07-10 08:16:13 -0700

    tiny cleanup to prevent defining some unused global variables when using TURB_DRIVING modules

--- allvars.h
+++ allvars.h
@@ -2119,12 +2119,13 @@ extern struct global_data_all_processes
     double MaxHsml;           /*!< minimum allowed gas kernel length */
 
 #ifdef TURB_DRIVING
-  double TurbInjectedEnergy;
-  double TurbDissipatedEnergy;
-  double TimeBetTurbSpectrum;
-  double TimeNextTurbSpectrum;
-  int FileNumberTurbSpectrum;
-  double SetLastTime;
+    double TurbInjectedEnergy;
+    double TurbDissipatedEnergy;
+#if defined(TURB_DRIVING_SPECTRUMGRID)
+    double TimeBetTurbSpectrum;
+    double TimeNextTurbSpectrum;
+    int FileNumberTurbSpectrum;
+#endif
 #endif
 
   double SofteningGas,		/*!< for type 0 */
