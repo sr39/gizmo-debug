e7c79b2378d8aee676bb1ac74fb028808c410c01
Ben Wibking <benwibking@gmail.com>
2020-11-29 17:21:16 +1100

    run as non-root in CI

--- run_testprob.sh
+++ run_testprob.sh
@@ -6,5 +6,5 @@ sed '/^#/d' < cooling/TREECOOL > TREECOOL
 ## download initial conditions
 wget "http://www.tapir.caltech.edu/~phopkins/sims/isodisk_ics.hdf5"
 
-## Bitbucket CI runs as root inside a Docker container, so it's ok
-mpirun --allow-run-as-root ./GIZMO scripts/test_problems/isodisk.params
+## run test problem
+mpirun ./GIZMO scripts/test_problems/isodisk.params
