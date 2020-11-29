028c0ca8553882d591d7d4024307800efb587d55
Ben Wibking <benwibking@gmail.com>
2020-11-29 17:04:01 +1100

    allow running as root during CI build

--- run_testprob.sh
+++ run_testprob.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## delete comments from TREECOOL file
 sed '/^#/d' < cooling/TREECOOL > TREECOOL
+
+## download initial conditions
 wget "http://www.tapir.caltech.edu/~phopkins/sims/isodisk_ics.hdf5"
-mpirun ./GIZMO scripts/test_problems/isodisk.params
+
+## Bitbucket CI runs as root inside a Docker container, so it's ok
+mpirun --allow-run-as-root ./GIZMO scripts/test_problems/isodisk.params
