baa3154c976c496560b78b803334c350787e6f2a
Ben Wibking <benwibking@gmail.com>
2020-11-30 15:47:28 +1100

    move pipeline script to scripts/pipelines

--- scripts/pipelines/run_isodisk_testprob.sh
+++ scripts/pipelines/run_isodisk_testprob.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+## emit Config.sh
+echo "HYDRO_MESHLESS_FINITE_MASS
+COOLING
+MULTIPLEDOMAINS=64
+ADAPTIVE_GRAVSOFT_FORGAS
+METALS
+GALSF
+GALSF_SFR_MOLECULAR_CRITERION
+GALSF_SFR_VIRIAL_SF_CRITERION=0" > Config.sh
+
+## re-build
+make clean && make
+
 ## delete comments from TREECOOL file
 sed '/^#/d' < cooling/TREECOOL > TREECOOL
 
