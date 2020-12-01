52517123992d51fd914a9b123e234ebd2face21d
Ben Wibking <benwibking@gmail.com>
2020-12-01 15:21:17 +1100

    updated pipeline script

--- scripts/pipelines/compare_isodisk_output.sh
+++ scripts/pipelines/compare_isodisk_output.sh
@@ -4,4 +4,4 @@
 wget -O snapshot_001.ref.hdf5 https://cloudstor.aarnet.edu.au/plus/s/5vIwL81CEm8uAlR/download
 
 ## compare at machine precision
-h5diff -v --use-system-epsilon output/snapshot_001.hdf5 snapshot_001.ref.hdf5
+h5diff -r --count=50 --use-system-epsilon output/snapshot_001.hdf5 snapshot_001.ref.hdf5
