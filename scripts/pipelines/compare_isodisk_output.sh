23cfd1859c44fcb9ac9c1a6d67b0ab7c7755ddae
Ben Wibking <benwibking@gmail.com>
2020-12-01 15:31:28 +1100

    adjust error tolerance for test suite

--- scripts/pipelines/compare_isodisk_output.sh
+++ scripts/pipelines/compare_isodisk_output.sh
@@ -4,4 +4,4 @@
 wget -O snapshot_001.ref.hdf5 https://cloudstor.aarnet.edu.au/plus/s/5vIwL81CEm8uAlR/download
 
 ## compare at machine precision
-h5diff -r --count=50 --use-system-epsilon output/snapshot_001.hdf5 snapshot_001.ref.hdf5
+h5diff -r --count=50 --relative=1.0E-07 output/snapshot_001.hdf5 snapshot_001.ref.hdf5
