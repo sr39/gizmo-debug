f40fe43794bf69d2a61261d5b7d532cc5afe07d4
Ben Wibking <benwibking@gmail.com>
2020-12-01 15:43:17 +1100

    remove bitbucket CI stuff for PR

--- scripts/pipelines/compare_isodisk_output.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## download reference solution
-wget -O snapshot_001.ref.hdf5 https://cloudstor.aarnet.edu.au/plus/s/5vIwL81CEm8uAlR/download
-
-## compare at machine precision
-h5diff -r --count=50 --relative=1.0E-07 output/snapshot_001.hdf5 snapshot_001.ref.hdf5
